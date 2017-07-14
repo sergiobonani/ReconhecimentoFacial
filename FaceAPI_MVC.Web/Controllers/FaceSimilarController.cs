7a42a5818e563d35d642153f3ba3f1e44a8bb790
Sérgio H Bonani <sergio.bonani@db1.com.br>
2017-07-14 17:40:26 -0300

    Teste de reconhecimento

---
+++ FaceAPI_MVC.Web/Controllers/FaceSimilarController.cs
@@ -0,0 +1,337 @@
+﻿using FaceAPI_MVC.Web.Helper;
+using FaceAPI_MVC.Web.Models;
+using Microsoft.ProjectOxford.Face;
+using Microsoft.ProjectOxford.Face.Contract;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FaceAPI_MVC.Web.Controllers
+{
+    public class FaceSimilarController : Controller
+    {
+        private static string ServiceKey = ConfigurationManager.AppSettings["FaceServiceKey"];
+        private static string directory = "../UploadedFiles";
+        private static string _faceListName = string.Empty;
+        private static ObservableCollection<vmFace> _facesCollection = new ObservableCollection<vmFace>();
+        private static ObservableCollection<vmFindSimilarResult> _findSimilarCollection = new ObservableCollection<vmFindSimilarResult>();
+
+        public ObservableCollection<vmFace> FacesCollection
+        {
+            get
+            {
+                return _facesCollection;
+            }
+        }
+        public ObservableCollection<vmFindSimilarResult> FindSimilarCollection
+        {
+            get
+            {
+                return _findSimilarCollection;
+            }
+        }
+
+
+        // GET: FaceSimilar
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> SaveCandidateFiles()
+        {
+            string message = string.Empty, fileName = string.Empty, actualFileName = string.Empty; bool flag = false;
+
+            //Requested File Collection
+            HttpFileCollection fileRequested = System.Web.HttpContext.Current.Request.Files;
+            if (fileRequested != null)
+            {
+                //Create New Folder
+                CreateDirectory();
+
+                //Clear Existing File in Folder
+                ClearDirectory();
+
+                for (int i = 0; i < fileRequested.Count; i++)
+                {
+                    var file = Request.Files[i];
+                    actualFileName = file.FileName;
+                    fileName = Guid.NewGuid() + Path.GetExtension(file.FileName);
+                    int size = file.ContentLength;
+                    string FullImgPath = Path.Combine(Server.MapPath(directory), fileName);
+                    try
+                    {
+                        file.SaveAs(FullImgPath);
+                        message = "File uploaded successfully";
+                        flag = true;
+
+                        if (FullImgPath != "")
+                        {
+                            using (var fStream = System.IO.File.OpenRead(FullImgPath))
+                            {
+                                // User picked one image
+                                var imageInfo = UIHelper.GetImageInfoForRendering(FullImgPath);
+
+                                // Create Instance of Service Client by passing Servicekey as parameter in constructor 
+                                var faceServiceClient = new FaceServiceClient(ServiceKey);
+                                Face[] faces = await faceServiceClient.DetectAsync(fStream, true, true, new FaceAttributeType[] { FaceAttributeType.Gender, FaceAttributeType.Age, FaceAttributeType.Smile, FaceAttributeType.Glasses });
+
+                                if (faces.Count() > 0)
+                                {
+                                    Bitmap CroppedFace = null;
+                                    foreach (var face in faces)
+                                    {
+                                        //Create & Save Cropped Images
+                                        var croppedImg = Convert.ToString(Guid.NewGuid()) + ".jpeg" as string;
+                                        var croppedImgPath = directory + '\\' + croppedImg as string;
+                                        var croppedImgFullPath = Server.MapPath(directory) + '\\' + croppedImg as string;
+                                        CroppedFace = CropBitmap(
+                                                        (Bitmap)Image.FromFile(FullImgPath),
+                                                        face.FaceRectangle.Left,
+                                                        face.FaceRectangle.Top,
+                                                        face.FaceRectangle.Width,
+                                                        face.FaceRectangle.Height);
+                                        CroppedFace.Save(croppedImgFullPath, ImageFormat.Jpeg);
+                                        if (CroppedFace != null)
+                                            ((IDisposable)CroppedFace).Dispose();
+                                    }
+
+                                    //Clear Query File
+                                    DeleteFile(FullImgPath);
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        message = "File upload failed! Please try again";
+                    }
+                }
+            }
+            return new JsonResult
+            {
+                Data = new
+                {
+                    Message = message,
+                    Status = flag
+                }
+            };
+        }
+
+        [HttpGet]
+        public async Task<dynamic> GetCandidateFiles()
+        {
+            string message = string.Empty;
+            var faceServiceClient = new FaceServiceClient(ServiceKey);
+            FacesCollection.Clear();
+            DirectoryInfo dir = new DirectoryInfo(Path.Combine(Server.MapPath(directory)));
+            FileInfo[] files = null;
+            files = dir.GetFiles().OrderBy(p => p.CreationTime).ToArray();
+
+            if (files.Count() > 0)
+            {
+                _faceListName = Guid.NewGuid().ToString();
+                await faceServiceClient.CreateFaceListAsync(_faceListName, _faceListName, "face list for sample");
+
+                foreach (var item in files)
+                {
+                    var imgPath = Server.MapPath(directory) + '\\' + item.Name as string;
+                    try
+                    {
+                        using (var fStream = System.IO.File.OpenRead(imgPath))
+                        {
+                            var faces = await faceServiceClient.AddFaceToFaceListAsync(_faceListName, fStream);
+                            FacesCollection.Add(new vmFace
+                            {
+                                ImagePath = imgPath,
+                                FileName = item.Name,
+                                FilePath = directory + '\\' + item.Name,
+                                FaceId = Convert.ToString(faces.PersistedFaceId)
+                            });
+                        }
+                    }
+                    catch (FaceAPIException fe)
+                    {
+                        //do exception work
+                        message = fe.ToString();
+                    }
+                }
+
+            }
+            else
+            {
+                message = "No files to Detect!! Please Upload Files";
+            }
+            return new JsonResult
+            {
+                Data = new
+                {
+                    Message = message,
+                    FacesCollection = FacesCollection
+                },
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet
+            };
+        }
+
+        [HttpPost]
+        public async Task<dynamic> FindSimilar()
+        {
+            string message = string.Empty, fileName = string.Empty, actualFileName = string.Empty; bool flag = false;
+            var faceServiceClient = new FaceServiceClient(ServiceKey);
+            FindSimilarCollection.Clear();
+
+            //Requested File Collection
+            HttpFileCollection fileRequested = System.Web.HttpContext.Current.Request.Files;
+
+            if (fileRequested != null)
+            {
+                for (int i = 0; i < fileRequested.Count; i++)
+                {
+                    var file = Request.Files[i];
+                    actualFileName = file.FileName;
+                    fileName = Guid.NewGuid() + Path.GetExtension(file.FileName);
+                    int size = file.ContentLength;
+
+                    try
+                    {
+                        file.SaveAs(Path.Combine(Server.MapPath(directory), fileName));
+                        var imgPath = Server.MapPath(directory) + '/' + fileName as string;
+                        using (var fStream = System.IO.File.OpenRead(imgPath))
+                        {
+                            var faces = await faceServiceClient.DetectAsync(fStream);
+
+                            //Find similar faces for each face
+                            foreach (var f in faces)
+                            {
+                                var faceId = f.FaceId;
+                                try
+                                {
+                                    //Call find similar REST API, the result contains all the face ids which similar to the query face
+                                    const int requestCandidatesCount = 10;
+                                    var result = await faceServiceClient.FindSimilarAsync(faceId, _faceListName, requestCandidatesCount);
+
+                                    var findResult = new vmFindSimilarResult();
+                                    findResult.Faces = new ObservableCollection<vmFace>();
+                                    findResult.QueryFace = new vmFace()
+                                    {
+                                        ImagePath = imgPath,
+                                        FileName = fileName,
+                                        FilePath = directory + '/' + fileName,
+                                        Top = f.FaceRectangle.Top,
+                                        Left = f.FaceRectangle.Left,
+                                        Width = f.FaceRectangle.Width,
+                                        Height = f.FaceRectangle.Height,
+                                        FaceId = faceId.ToString(),
+                                    };
+
+                                    //Update find similar results collection for rendering
+                                    foreach (var fr in result)
+                                    {
+                                        findResult.Faces.Add(FacesCollection.First(ff => ff.FaceId == fr.PersistedFaceId.ToString()));
+                                    }
+
+                                    //Update UI
+                                    FindSimilarCollection.Add(findResult);
+                                    message = Convert.ToString("Total " + findResult.Faces.Count() + " faces are detected.");
+                                    flag = true;
+                                }
+                                catch (FaceAPIException fex)
+                                {
+                                    message = fex.ErrorMessage;
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        ex.ToString();
+                    }
+                }
+            }
+            return new JsonResult
+            {
+                Data = new
+                {
+                    Message = message,
+                    SimilarFace = FindSimilarCollection,
+                    Status = flag
+                }
+            };
+        }
+
+        public Bitmap CropBitmap(Bitmap bitmap, int cropX, int cropY, int cropWidth, int cropHeight)
+        {
+            Rectangle rect = new Rectangle(cropX, cropY, cropWidth, cropHeight);
+            Bitmap cropped = bitmap.Clone(rect, bitmap.PixelFormat);
+            return cropped;
+        }
+
+        public void CreateDirectory()
+        {
+            bool exists = System.IO.Directory.Exists(Server.MapPath(directory));
+            if (!exists)
+            {
+                try
+                {
+                    Directory.CreateDirectory(Server.MapPath(directory));
+                }
+                catch (Exception ex)
+                {
+                    ex.ToString();
+                }
+            }
+        }
+
+        public void ClearDirectory()
+        {
+            DirectoryInfo dir = new DirectoryInfo(Path.Combine(Server.MapPath(directory)));
+            var files = dir.GetFiles();
+            if (files.Length > 0)
+            {
+                try
+                {
+                    foreach (FileInfo fi in dir.GetFiles())
+                    {
+                        GC.Collect();
+                        GC.WaitForPendingFinalizers();
+                        fi.Delete();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ex.ToString();
+                }
+            }
+        }
+
+        public void DeleteFile(string FullImgPath)
+        {
+            if (FullImgPath != "")
+            {
+                try
+                {
+                    //Clear Query File
+                    if ((System.IO.File.Exists(FullImgPath)))
+                    {
+                        GC.Collect();
+                        GC.WaitForPendingFinalizers();
+                        System.IO.File.Delete(FullImgPath);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ex.ToString();
+                }
+            }
+        }
+    }
+}
---
+++ FaceAPI_MVC.Web/ScriptsNg/FaceSimilarCtrl.js
@@ -0,0 +1,99 @@
+﻿angular.module('myFaceApp', [])
+.controller('faceSimilarCtrl', function ($scope, FileUploadService) {
+    $scope.Title = 'Microsoft FaceAPI - Find Face Similar';
+    $scope.resultMessage = 'No result found!!';
+    $scope.SelectedFileForUpload = null;
+    $scope.UploadedFiles = [];
+    $scope.SimilarFace = [];
+
+    //File Select & Save 
+    $scope.selectCandidateFileforUpload = function (file) {
+        $scope.SelectedFileForUpload = file;
+        $scope.loaderMoreupl = true;
+        $scope.uplMessage = 'Uploading, please wait....!';
+        $scope.result = "color-red";
+
+        //Save File
+        var uploaderUrl = "/FaceSimilar/SaveCandidateFiles";
+        var fileSave = FileUploadService.UploadFile($scope.SelectedFileForUpload, uploaderUrl);
+        fileSave.then(function (response) {
+            if (response.data.Status) {
+                $scope.GetCandidateFile();
+                angular.forEach(angular.element("input[type='file']"), function (inputElem) {
+                    angular.element(inputElem).val(null);
+                });
+                $scope.f1.$setPristine();
+                //$scope.uplMessage = response.data.Message;
+                $scope.loaderMoreupl = false;
+            }
+        },
+        function (error) {
+            console.warn("Error: " + error);
+        });
+    }
+
+    $scope.GetCandidateFile = function () {
+        $scope.loaderMore = true;
+        $scope.faceMessage = 'Preparing, please wait....!';
+        $scope.result = "color-red";
+
+        var fileUrl = "/FaceSimilar/GetCandidateFiles";
+        var fileView = FileUploadService.GetUploadedFile(fileUrl);
+        fileView.then(function (response) {
+            $scope.UploadedFiles = response.data.FacesCollection;
+            $scope.resultFaceMessage = response.data.Message;
+            $scope.loaderMore = false;
+        },
+        function (error) {
+            console.warn("Error: " + error);
+        });
+    };
+
+    $scope.selectFileforFindSimilar = function (file) {
+        $scope.SelectedFileForUpload = file;
+        $scope.loaderMorefacefinder = true;
+        $scope.facefinderMessage = 'Preparing, detecting faces, please wait....!';
+        $scope.result = "color-red";
+
+        //Find Similar Face
+        var uploaderUrl = "/FaceSimilar/FindSimilar";
+        var fileSave = FileUploadService.UploadFile($scope.SelectedFileForUpload, uploaderUrl);
+        fileSave.then(function (response) {
+            if (response.data.Status) {
+                $scope.QueryFace = response.data.SimilarFace[0].QueryFace.FilePath;
+                $scope.SimilarFace = response.data.SimilarFace[0].Faces;
+                angular.forEach(angular.element("input[type='file']"), function (inputElem) {
+                    angular.element(inputElem).val(null);
+                });
+                $scope.f2.$setPristine();
+                $scope.resultMessage = response.data.Message;
+                $scope.loaderMoreupl = false;
+            }
+        },
+        function (error) {
+            console.warn("Error: " + error);
+        });
+    }
+})
+.factory('FileUploadService', function ($http, $q) {
+    var fact = {};
+    fact.UploadFile = function (files, uploaderUrl) {
+        var formData = new FormData();
+        angular.forEach(files, function (f, i) {
+            formData.append("file", files[i]);
+        });
+        var request = $http({
+            method: "post",
+            url: uploaderUrl,
+            data: formData,
+            withCredentials: true,
+            headers: { 'Content-Type': undefined },
+            transformRequest: angular.identity
+        });
+        return request;
+    }
+    fact.GetUploadedFile = function (fileUrl) {
+        return $http.get(fileUrl);
+    }
+    return fact;
+})
